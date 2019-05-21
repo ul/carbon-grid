a9ec655e22f0b282230545508b89514eeaabbca2
Ruslan Prokopchuk <fer.obbee@gmail.com>
2019-05-21 21:07:35 +1000

    demo

--- test/carbon_grid/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns carbon-grid.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [carbon-grid.core :refer [init-grid]]))
-
-(deftest init-grid-test
-  (is (= (init-grid 2 2) [[[0 0] [0 1]] [[1 0] [1 1]]])))
